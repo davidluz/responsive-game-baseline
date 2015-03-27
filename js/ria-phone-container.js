d92f62e49c96e342c37e60bbfdf27e8ff3fdb95c
David Luz <davidmelo8@hotmail.com>
2015-03-27 11:57:44 -0300

    V1

---
+++ js/ria-phone-container.js
@@ -0,0 +1,23 @@
+/*Set de default game size*/
+gameWidth = 320;
+gameHeight = 240;
+
+
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, 'ria-phone-container', { preload: preload, create: create, update: update});
+
+
+
+function preload(){
+
+
+}
+
+function create(){
+
+
+}
+
+function update(){
+
+
+}
---
+++ js/ria-tablet-container.js
@@ -0,0 +1,23 @@
+/*Set de default game size*/
+gameWidth = 680;
+gameHeight = 400;
+
+
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, 'ria-tablet-container', { preload: preload, create: create, update: update});
+
+
+
+function preload(){
+
+
+}
+
+function create(){
+
+
+}
+
+function update(){
+
+
+}
